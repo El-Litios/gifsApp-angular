50c86aca92fe7ae5d73befafaeb101b32cebff2b
El-Litios <carlosguzmanrojas00@gmail.com>
2023-03-26 00:15:17 -0300

    import service

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-search',
@@ -8,8 +9,18 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class SearchComponent {
   //! para decir que el objeto nunca sera nulo
   @ViewChild('textSearch') textSearch!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifService: GifsService) {}
+
   search() {
-    console.log(this.textSearch.nativeElement.value);
+    //console.log(this.textSearch.nativeElement.value);
+    const value = this.textSearch.nativeElement.value;
+
+    if (value.trim().length == 0) {
+      return;
+    }
+
+    this.gifService.searchGifs(value);
 
     this.textSearch.nativeElement.value = '';
   }
